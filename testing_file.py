a566958c4b3e449133098ad98faf10ddeca6cd3e
maujordan <mau_ja99@hotmail.com>
2022-12-05 20:20:57 -0600

    minor changes

--- testing_file.py
+++ testing_file.py
@@ -1,26 +1,7 @@
-# uvicorn app:app --> Inicia el servidor de nombre app
-# uvicorn app:app --reload --> Cada vez que guarda el archivo refresca el servidor uvicorn 
-# 
+import os
 
-from fastapi import FastAPI, Request, BackgroundTasks
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel
-import uvicorn
-import inspect
-# User functions
-from main_send_recording import broadcast_recording
-from main_record_artnet import record_artnet
-import json
-from utils.utils import *
-
-
-level = ["settings", "ip_address"]
-config_path = "./config.json"
-value = "192.168.1.65"
-    
-        
-
-
-
+devices = []
+for device in os.popen('arp -a'): 
+    devices.append(device)
+    print(device)
 
